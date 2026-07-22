8febe7e653fffed3eb8a32682776714cdaacba81
reubenson adat <reubensonadat0@gmail.com>
2026-07-22 14:41:58 +0000

    revert: restore body scroll lock when modals are open

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -39,10 +39,12 @@ const Modal = ({
 
     if (isOpen) {
       document.addEventListener('keydown', handleEscape);
+      document.body.style.overflow = 'hidden';
     }
 
     return () => {
       document.removeEventListener('keydown', handleEscape);
+      document.body.style.overflow = 'unset';
     };
   }, [isOpen, onClose]);
 
